7fa63ccb54bbd22a0c5c9b1b0ee3fe4d61d1c5ad
yuya <nerentabelen@mail.ru>
2025-09-09 18:21:20 +0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Влад Рыжкин on 09.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memorizeTests/memorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeTests.swift
+//  memorizeTests
+//
+//  Created by Влад Рыжкин on 09.09.2025.
+//
+
+import Testing
+@testable import memorize
+
+struct memorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
