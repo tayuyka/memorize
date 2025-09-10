741f1626d8f5acdefe1cb2b7a7e483fbc037ff48
yuya <nerentabelen@mail.ru>
2025-09-10 16:11:03 +0300

    add state object into entry point

--- memorize/App/memorizeApp.swift
+++ memorize/App/memorizeApp.swift
@@ -3,11 +3,14 @@ import SwiftUI
 
 @main
 struct memorizeApp: App {
+    @StateObject private var themeManager = ThemeManager(repo: InMemoryThemeRepository())
+
     var body: some Scene {
         WindowGroup {
             NavigationStack {
                 MainMenuView()
             }
+            .environmentObject(themeManager)
         }
     }
 }
