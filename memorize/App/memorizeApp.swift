5b31e4d0da4a92c54d138a4161793284febd78ea
yuya <nerentabelen@mail.ru>
2025-09-12 19:39:10 +0300

    add init sattigs for white toolbar elements

--- memorize/App/memorizeApp.swift
+++ memorize/App/memorizeApp.swift
@@ -4,7 +4,25 @@ import SwiftUI
 @main
 struct memorizeApp: App {
     @StateObject private var themeManager = ThemeManager(repo: InMemoryThemeRepository())
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        appearance.backgroundColor = .clear
+        appearance.shadowColor = .clear
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
 
+        let nav = UINavigationBar.appearance()
+        nav.standardAppearance = appearance
+        nav.scrollEdgeAppearance = appearance
+        nav.compactAppearance = appearance
+
+        nav.tintColor = .white
+        UIBarButtonItem.appearance().tintColor = .white
+    }
+
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
